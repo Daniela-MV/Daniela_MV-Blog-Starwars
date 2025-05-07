38d656340d2392448630e4cd883f189ce59b36c7
Daniela-MV <danmv.0991@gmail.com>
2025-05-07 23:00:58 +0000

    diseño

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,14 +3,23 @@ import { Link } from "react-router-dom";
 export const Navbar = () => {
 
 	return (
-		<nav className="navbar navbar-light bg-light">
+		<nav className="navbar" style={{backgroundColor: "black", width: "100%"}} >
 			<div className="container">
 				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+					<img className="imagenStarwars" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Star_Wars_Logo.svg/800px-Star_Wars_Logo.svg.png" alt="starwars" />
 				</Link>
 				<div className="ml-auto">
 					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
+						<div className="btn-group">
+							<button type="button" className="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+							Favoritos <i className="bi bi-star" style={{color: "yellow"}}></i>
+							</button>
+							<ul className="dropdown-menu">
+								<li><a className="dropdown-item" href="#">Action</a></li>
+								<li><a className="dropdown-item" href="#">Another action</a></li>
+								<li><a className="dropdown-item" href="#">Something else here</a></li>
+							</ul>
+						</div>
 					</Link>
 				</div>
 			</div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,23 @@
-import rigoImageUrl from "../assets/img/rigo-baby.jpg";
+
 import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
 
 export const Home = () => {
 
-  const {store, dispatch} =useGlobalReducer()
+	const { store, dispatch } = useGlobalReducer()
 
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
+		<div className="carta m-5">
+			<div className="card" style={{backgroundColor:"rgba(247,201,62)", width:"18rem"}}>
+				<img src="https://www.uv.es/loferma2/images/wi.jpg" className="card-img-top" alt="" style={{height:"300px"}}/>
+					<div className="card-body">
+						<h5 className="card-title">Card title</h5>
+						<p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+						<div className= "d-flex justify-content-between">
+						<button type="button" className="btn btn-dark">Learn More</button>
+						<button type="button" className="btn btn-dark"><i className="bi bi-heart" style={{color:"yellow"}} ></i></button>
+						</div>
+					</div>
+			</div>
 		</div>
 	);
 }; 
